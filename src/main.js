13801fb1b9daf208738c2606d4a3b9581713ed58
Werayut Kumsuwan <9werayut@Werayuts-MacBook-Pro.local>
2017-06-17 09:23:48 +0700

    first commit to-do master app

---
+++ src/main.js
@@ -0,0 +1,97 @@
+import React, { Component } from 'react';
+import {
+    StyleSheet,
+    Text,
+    TextInput,
+    View,
+    TouchableOpacity
+} from 'react-native';
+
+module.exports = React.createClass({
+    getInitialState() {
+        return ({
+            tasks: ['Take out the trash', 'Get groceries', 'Practice piano'],
+            task: ''
+        })
+    },
+
+    renderList(tasks) {
+        return (
+            tasks.map((task, index) => {
+                return(
+                    <View key={task} style={styles.task}>
+                        <Text>
+                            {task}
+                        </Text>
+                        <TouchableOpacity
+                            onPress={() => this.completeTask(index)}
+                        >
+                            <Text>
+                                &#10003;
+                            </Text>
+                        </TouchableOpacity>
+                    </View>
+                )
+            })
+        )
+    },
+
+    completeTask(index) {
+        console.log('complete a task: ', this.state.tasks[index]);
+        let tasks = this.state.tasks;
+        tasks = tasks.slice(0, index).concat(tasks.slice(index + 1));
+        this.setState({tasks});
+    },
+
+    addTask() {
+        let tasks = this.state.tasks.concat([this.state.task]);
+        this.setState({tasks});
+    },
+    
+    render() {
+        return (
+                <View style={styles.container}>
+                    <Text style={styles.header}>
+                        To-Do Master
+                    </Text>
+                    <TextInput 
+                        style={styles.input}
+                        placeholder="Add a task..."
+                        onChangeText={(text) => {
+                            this.setState({task: text});
+                            console.log(this.state.task);
+                        }}
+                        onEndEditing={() => this.addTask()}
+                    />
+                    {this.renderList(this.state.tasks)}
+                </View>
+        )
+    }
+})
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1
+    },
+    header: {
+        margin: 30,
+        marginTop: 40,
+        textAlign: 'center',
+        fontSize: 18
+    },
+    task: {
+        height: 60,
+        borderBottomWidth: 1,
+        borderColor: 'black',
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    input: {
+        height: 60,
+        borderWidth: 1,
+        borderRadius: 5,
+        borderColor: 'black',
+        textAlign: 'center',
+        margin: 10
+    }
+})
